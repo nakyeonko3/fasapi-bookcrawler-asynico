3f2ca829def766bd257dce598e8751ab3134c8d9
nakyeonko3 <badaclock@gmail.com>
2024-02-03 23:19:43 +0900

    add : item.html

--- app/main.py
+++ app/main.py
@@ -1,14 +1,28 @@
-from typing import Optional
-from fastapi import FastAPI
+# from typing import Optional
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent
 
 app = FastAPI()
+templates = Jinja2Templates(directory=BASE_DIR / "templates")
 
 
-@app.get("/")
-def read_root():
-    return {"Hello", "World"}
+@app.get("/item/{id}", response_class=HTMLResponse)
+def read_item(request: Request, id: str):
+    return templates.TemplateResponse(
+        "./item.html",
+        {
+            "request": request,
+            "id": id,
+        },
+    )
 
 
-@app.get("/item/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+# app = FastAPI()
+# - 객체지향 싱글톤 패턴
+# @app.get("/")
+# - 라우터
+# - @app.ge 데코레이터
